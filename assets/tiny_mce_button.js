560c9750475df0f66c7c6eb5c7beab470ea7b306
Shahjahan Jewel <cep.jewel@gmail.com>
2018-05-19 11:36:24 +0600

    added

--- assets/tiny_mce_button.js
+++ assets/tiny_mce_button.js
@@ -1,12 +1,24 @@
 ( function() {
+    jQuery('body').append(`
+        <div style="display: none;width: 100%;position: absolute;right: 50%;top: 0%;text-align: right;z-index: 99999999999;" class="modal">
+            <input type="text" id="shortcode_custom" />
+            <button id="new_shortcode_button">hello There</button>
+        </div>
+    `);
+    
     tinymce.PluginManager.add( 'res_ninja_shortcodes', function( editor, url ) {
-
         editor.addButton( 'shortCode', {
-
             text: 'Restaurant Menu ShortCode',
             icon: false,
             onclick: function() {
-                // Open window
+                jQuery('.modal').css('display', 'block');
+                
+                jQuery('#new_shortcode_button').on('click', () => {
+                    let value = jQuery('#shortcode_custom').val();
+                    editor.insertContent('['+value+']');
+                });
+                
+                return;
                 editor.windowManager.open( {
                     title: 'Select ShortCode',
                     body: [
@@ -75,17 +87,14 @@
                 ],
                 onsubmit: function( e ) {
                     // Insert content when the window form is submitted
-                    editor.setContent('');
-                    // console.log(e.data);
+                   // editor.setContent('');
+                     console.log(e.data);
                     editor.insertContent( 
                         '[restaurant_menu display="' + e.data.display +'" meal_type="'+e.data.meal_type+'" dish_type="'+e.data.dish_type+'" location="'+e.data.location+'"]'
                     );
                 }
             });
             }
-
         } );
-
     } );
-
 } )();
